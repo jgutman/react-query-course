b108b807ce8acb459c3f0f3324392dae92cd7ccf
Jackie Gutman <jacqueline.gutman@rstudio.com>
2023-06-28 11:03:42 -0400

    throw helpful error

--- src/helpers/useUserData.js
+++ src/helpers/useUserData.js
@@ -1,6 +1,10 @@
 import { useQuery } from "@tanstack/react-query";
 
 export function useUserData(userId) {
+    if (!userId) {
+        throw new Error("useUserData must be called with a userId");
+    }
+
     const queryKey = ["users", userId];
     const queryFn = () => fetch(`/api/users/${userId}`).then((res) => res.json());
 
